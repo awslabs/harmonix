19e870f25879c5f211f1c31ddcf87404e1b7195e
Jim McVea <117947708+awsjim@users.noreply.github.com>
2023-09-28 22:48:20 -0400

    Install platform issues (#38)
    * fix: intermittent env replacement failure
    
    * fix: encrypt rds resources in sample

--- build-script/destroy-platform.sh
+++ build-script/destroy-platform.sh
@@ -16,10 +16,10 @@ fi
 # if AWS_DEFAULT_REGION is not set, ask for it
 if [[ -z $AWS_DEFAULT_REGION ]]; then
   defaultRegion=$( if [[ -n "${AWS_REGION}" ]]; then echo ${AWS_REGION}; else aws configure get region; fi )
-  aws_region AWS_REGION "Enter the AWS region where the OPA platform was deployed:" "$defaultRegion"
+  aws_region AWS_DEFAULT_REGION "Enter the AWS region where the OPA platform was deployed:" "$defaultRegion"
 fi
 
 cd $opaPlatformDir
 confirm_aws_account
-cdk destroy --account $AWS_ACCOUNT_ID --region $AWS_REGION --progress bar
+cdk destroy --account $AWS_ACCOUNT_ID --region $AWS_DEFAULT_REGION --progress bar
 cd -
---
+++ build-script/set-gitlab-token.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+configDir=$scriptDir/../config
+
+source $configDir/.env
+
+GITLAB_API_TOKEN=$(aws secretsmanager get-secret-value --secret-id opa-admin-gitlab-secrets --output text --query 'SecretString' | jq -r '.apiToken')
+echo "GITLAB_API_TOKEN=$GITLAB_API_TOKEN"
+sed -i.bak "s/^\(SECRET_GITLAB_CONFIG_PROP_apiToken=\).*$/\1\"$GITLAB_API_TOKEN\"/g" ./config/.env && rm -f ./config/.env.bak

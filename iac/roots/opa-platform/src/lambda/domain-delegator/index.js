6d793b1176d2b85508c12dbe7026cb40c975d61b
simon <sgebres@amazon.com>
2023-10-11 16:43:44 -0400

    updated custom lambda with delete method

--- iac/roots/opa-platform/src/lambda/domain-delegator/index.js
+++ iac/roots/opa-platform/src/lambda/domain-delegator/index.js
@@ -1,18 +1,19 @@
 const AWS = require("aws-sdk");
 const https = require("https");
-const { URL } = require("url");
 
-const handler = async (event, context) => {
-  //const responseUrl = event.ResponseURL || null;
-  const zoneId = process.env.ZONE_ID;
+const ZONE_ID = process.env.ZONE_ID;
+const HOSTNAME = "lcwe7lqhzh.execute-api.us-east-1.amazonaws.com";
+const PATHNAME = "/dev/processUserData";
 
+const handler = async (event) => {
   switch (event.RequestType) {
     case "Update":
     case "Create":
-      await registerDns(zoneId);
+      await registerDns(ZONE_ID);
       break;
     case "Delete":
-      return;
+      await deregisterDns(ZONE_ID);
+      break;
     default:
       throw new Error("Unknown request type");
   }
@@ -20,7 +21,6 @@ const handler = async (event, context) => {
 
 async function registerDns(zoneId) {
   const r53 = new AWS.Route53();
-  console.log(`m zone id is ======= ${zoneId}`);
 
   const response = await r53
     .listResourceRecordSets({
@@ -28,123 +28,93 @@ async function registerDns(zoneId) {
     })
     .promise();
 
-  const nameServerRecord = response.ResourceRecordSets.find(
-    (record) => record.Type === "NS"
-  );
+  const nameServerRecord = response.ResourceRecordSets.find((record) => record.Type === "NS");
 
-  const nameServers =
-    nameServerRecord.ResourceRecords?.map((r) => r.Value).join(" ") || "";
-  console.log(`my name servers are  is ======= ${nameServers}`);
-  console.log(`m zone name is ======= ${nameServerRecord.Name}`);
+  if (!nameServerRecord) {
+    console.log("No NS record found.");
+    return;
+  }
 
-  //   if (!nameServerRecord) {
-  //     console.log(`Unable to find ns record in ${response}`);
-  //     return;
-  //   }
-  // } catch (e) {
-  //   console.error("Error listing record set hosted zone", e.message);
-  // }
-
-  // const nameServers =
-  //   nameServerRecord.ResourceRecords?.map((r) => r.Value).join(" ") || "";
-
-  const data = JSON.stringify({
+  const data = {
     Domain: nameServerRecord.Name,
-    NameServers: nameServers,
-  });
-
-  const hostname = "lcwe7lqhzh.execute-api.us-east-1.amazonaws.com";
-  const pathname = "/dev/processUserData";
-
-  const options = {
-    method: "POST",
-    hostname: hostname,
-    path: pathname,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    maxRedirects: 20,
+    NameServers: nameServerRecord.ResourceRecords.map((r) => r.Value).join(" "),
   };
 
+  await sendHttpRequest("POST", data);
+}
+
+async function deregisterDns(zoneId) {
+  const r53 = new AWS.Route53();
+
+  const response = await r53
+    .listResourceRecordSets({
+      HostedZoneId: `/hostedzone/${zoneId}`,
+    })
+    .promise();
+
+  // Delete CNAME records
+  const cnameRecords = response.ResourceRecordSets.filter((record) => record.Type === "CNAME");
+  for (const record of cnameRecords) {
+    await r53
+      .changeResourceRecordSets({
+        HostedZoneId: `/hostedzone/${zoneId}`,
+        ChangeBatch: {
+          Changes: [
+            {
+              Action: "DELETE",
+              ResourceRecordSet: record,
+            },
+          ],
+        },
+      })
+      .promise();
+    console.log(`Deleted CNAME record: ${record.Name}`);
+  }
+
+  const data = {
+    Domain: response.ResourceRecordSets.find((record) => record.Type === "NS")?.Name,
+    NameServers: response.ResourceRecordSets.find((record) => record.Type === "NS")
+      ?.ResourceRecords.map((r) => r.Value)
+      .join(" "),
+  };
+
+  if (!data.Domain || !data.NameServers) {
+    console.log("No NS record found to delete.");
+    return;
+  }
+
+  await sendHttpRequest("PATCH", data);
+}
+
+function sendHttpRequest(method, data) {
   return new Promise((resolve, reject) => {
-    const req = https.request(options, function (res) {
+    const options = {
+      method,
+      hostname: HOSTNAME,
+      path: PATHNAME,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      maxRedirects: 20,
+    };
+
+    const req = https.request(options, (res) => {
       const chunks = [];
-
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
+      res.on("data", (chunk) => chunks.push(chunk));
+      res.on("end", () => {
+        const body = Buffer.concat(chunks).toString();
+        console.log(`Response from ${method} request:`, body);
+        resolve(body);
       });
-
-      res.on("end", function (chunk) {
-        const body = Buffer.concat(chunks);
-        resolve(body.toString());
-        console.log(body.toString());
-      });
-
-      res.on("error", function (error) {
-        console.error(error);
+      res.on("error", (error) => {
+        console.error(`Error in ${method} request:`, error);
+        reject(error);
       });
     });
-    req.write(data);
+
+    req.write(JSON.stringify(data));
     req.end();
   });
 }
-
-// async function sendCfnResponse(
-//   responseUrl,
-//   event,
-//   context,
-//   success,
-//   data = {}
-// ) {
-//   const status = success ? "SUCCESS" : "FAILED";
-//   const responseData = {
-//     StackId: event.StackId || null,
-//     RequestId: event.RequestId || null,
-//     LogicalResourceId: event.LogicalResourceId || "",
-//     PhysicalResourceId:
-//       event.PhysicalResourceId ||
-//       `${context.functionName}-${context.functionVersion}`,
-//     Status: status,
-//     Data: data,
-//   };
-
-//   const body = JSON.stringify(responseData);
-//   const headers = {
-//     "content-type": "",
-//     "content-length": body.length.toString(),
-//   };
-
-//   console.log("SENDING RESPONSE");
-//   console.log(body);
-
-//   return new Promise((resolve, reject) => {
-//     //const url = new URL(responseUrl);
-//     const url = responseUrl;
-//     const req = https.request(
-//       {
-//         method: "PUT",
-//         protocol: url.protocol,
-//         hostname: url.hostname,
-//         path: url.pathname + url.search,
-//         headers: headers,
-//       },
-//       (res) => {
-//         if (res.statusCode !== 200) {
-//           reject(new Error(`Failed with status code: ${res.statusCode}`));
-//         } else {
-//           resolve();
-//         }
-//       }
-//     );
-
-//     req.on("error", (e) => {
-//       console.error(`send(..) failed executing requests.put(..): ${e}`);
-//       reject(e);
-//     });
-
-//     req.write(body);
-//     req.end();
-//   });
-// }
 
 exports.handler = handler;

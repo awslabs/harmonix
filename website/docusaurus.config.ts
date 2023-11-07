5d658b72e224d323b1b90dd78f6621c1dc01b56e
Zahi Ben Shabat <zshabat@amazon.com>
2023-11-07 15:17:06 -0500

    change base url

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   url: 'https://opaonaws.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/opa-test/',
+  baseUrl: '/app-development-for-backstage-io-on-aws/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

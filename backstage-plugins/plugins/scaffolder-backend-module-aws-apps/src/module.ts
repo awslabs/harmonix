ef1958fb9c5abe45cb29a7102cf13af13c0be023
Florian JUDITH <florian.judith@alithya.com>
2024-10-17 20:03:44 -0400

    Added module for new backend system support
    Signed-off-by: Florian JUDITH <florian.judith@alithya.com>

---
+++ backstage-plugins/plugins/scaffolder-backend-module-aws-apps/src/module.ts
@@ -0,0 +1,51 @@
+import {
+    coreServices,
+    createBackendModule,
+  } from '@backstage/backend-plugin-api';
+  import { CatalogClient } from '@backstage/catalog-client';
+  import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+  import { ScmIntegrations } from '@backstage/integration';
+  import {
+    createRepoAccessTokenAction,
+    createSecretAction,
+    createS3BucketAction,
+    getEnvProvidersAction,
+    getComponentInfoAction,
+    getSsmParametersAction,
+    getPlatformParametersAction,
+    getPlatformMetadataAction,
+  } from './actions';
+  
+  /** @public */
+  export const scaffolderModuleAwsApps = createBackendModule({
+    pluginId: 'scaffolder', // name of the plugin that the module is targeting
+    moduleId: 'aws-apps',
+    register(env) {
+      env.registerInit({
+        deps: {
+          scaffolder: scaffolderActionsExtensionPoint,
+          config: coreServices.rootConfig,
+          discovery: coreServices.discovery,
+        },
+        async init({ scaffolder, config, discovery }) {
+          const integrations = ScmIntegrations.fromConfig(config);
+          const catalogClient = new CatalogClient({
+            discoveryApi: discovery,
+          });
+          scaffolder.addActions(createS3BucketAction());
+          scaffolder.addActions(createSecretAction({ envConfig: config }));
+          scaffolder.addActions(getEnvProvidersAction({ catalogClient }));
+          scaffolder.addActions(getComponentInfoAction());
+          scaffolder.addActions(getSsmParametersAction());
+          scaffolder.addActions(getPlatformMetadataAction({ envConfig: config }));
+          scaffolder.addActions(
+            getPlatformParametersAction({ envConfig: config }),
+          );
+          scaffolder.addActions(
+            createRepoAccessTokenAction({ integrations, envConfig: config }),
+          );
+        },
+      });
+    },
+  });
+  

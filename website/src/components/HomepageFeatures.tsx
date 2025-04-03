47641bdccd79dfb29e0df65da0d1a099313d126c
shub12388y <spl.sp999@gmail.com>
2025-04-03 19:33:23 +0530

    Redesign the hero section and resolve the types error in homepage
    Signed-off-by: shub12388y <spl.sp999@gmail.com>

--- website/src/components/HomepageFeatures.tsx
+++ website/src/components/HomepageFeatures.tsx
@@ -11,90 +11,91 @@ type FeatureGroup = {
 
 const opaMainVideoContent = (
   <div className='opa-video-item'>
-    <h4>The Platformers - Harmonix on AWS</h4>
-    <iframe width="400" height="225" src="https://www.youtube.com/embed/h9HwnkkmQPA/1.jpg?" title="What is Harmonix on AWS + Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
+  <h4>The Platformers - Harmonix on AWS</h4>
+  <iframe width="400" height="225" src="https://www.youtube.com/embed/h9HwnkkmQPA/1.jpg?" title="What is OPA on AWS + Demo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+</div> 
 )
 
 const opaSecondVideoContent = (
   <div className='opa-video-item'>
-    <h4>Harmonix on AWS at PlatformCon 2024</h4>
-    <iframe width="400" height="225" src="https://www.youtube.com/embed/cjovvIWdgRU/1.jpg" title="Unlocking Platform Engineer Efficiency: Automation Strategies with Backstage" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
+  <h4>Harmonix on AWS at PlatformCon 2024</h4>
+  <iframe width="400" height="225" src="https://www.youtube.com/embed/cjovvIWdgRU/1.jpg" title="Unlocking Platform Engineer Efficiency: Automation Strategies with Backstage" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+</div>
 )
 
 const opaThirdVideoContent = (
   <div className='opa-video-item'>
     <h4>Containers from the couch</h4>
-    <iframe width="400" height="225" src="https://www.youtube.com/embed/EN6mlTX71Ts/1.jpg?" title="Harmonix on AWS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="400" height="225" src="https://www.youtube.com/embed/EN6mlTX71Ts/1.jpg?" title="OPA on AWS" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
   </div>
 )
 const opaForthVideoContent = (
   <div className='opa-video-item'>
-    <h4>Harmonix on AWS at BackstageCon 2023</h4>
-    <iframe width="400" height="225" src="https://www.youtube.com/embed/40B6YmmhOvI/2.jpg?start=670" title="Let’s Go Backstage: End to End IDP Tips &amp; Tricks for Platform Engineers - Oshrat Nir &amp; Guy Menahem" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
+  <h4>Harmonix on AWS at BackstageCon 2023</h4>
+  <iframe width="400" height="225" src="https://www.youtube.com/embed/40B6YmmhOvI/2.jpg?start=670" title="Let’s Go Backstage: End to End IDP Tips &amp; Tricks for Platform Engineers - Oshrat Nir &amp; Guy Menahem" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+</div>
 )
 
 const opaFifthVideoContent = (
   <div className='opa-video-item'>
-    <h4>Harmonix on AWS Tech Playlist</h4>
-    {/* <iframe width="400" height="250" src="https://www.youtube.com/embed/videoseries?si=1ajMAQG975guA5ha&amp;list=PLhr1KZpdzukcf5e7vYOVkpw4h-rzy7Pn3"  title="Harmonix on AWS Playlist" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
-    <iframe width="400" height="225" src="https://www.youtube.com/embed/videoseries?si=1ajMAQG975guA5ha&amp;list=PLhr1KZpdzukcf5e7vYOVkpw4h-rzy7Pn3" title="Harmonix on AWS Playlist" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
+  <h4>Harmonix on AWS Tech Playlist</h4>
+  {/* <iframe width="400" height="250" src="https://www.youtube.com/embed/videoseries?si=1ajMAQG975guA5ha&amp;list=PLhr1KZpdzukcf5e7vYOVkpw4h-rzy7Pn3"  title="OPA on AWS Playlist" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+  <iframe width="400" height="225" src="https://www.youtube.com/embed/videoseries?si=1ajMAQG975guA5ha&amp;list=PLhr1KZpdzukcf5e7vYOVkpw4h-rzy7Pn3"  title="OPA on AWS Playlist" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+</div>
 )
 const opaSixthVideoContent = (
   <div className='opa-video-item'>
-    <h4>Container days Conference 2024 Demo</h4>
-    <iframe width="400" height="225" src="https://www.youtube.com/embed/Sg0X4RPJbvQ/2.jpg?start=1451" title="Building modern Architectures and Developer Acceleration" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
+  <h4>Container days Conference 2024 Demo</h4>
+  <iframe width="400" height="225" src="https://www.youtube.com/embed/Sg0X4RPJbvQ/2.jpg?start=1451" title="Building modern Architectures and Developer Acceleration" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+</div>
 )
 
 const carouselContent = (
   <div className='carouselTitle'>
-    <Carousel data-bs-theme="dark" interval={4200} pause={'hover'} touch={true} title='' >
-      <Carousel.Item>
-        <Carousel.Caption style={{ minHeight: '120px' }}>
-          <h4>🚀 NEW RELEASE 0.3.4 🚀</h4>
-          <p><b>V0.3.4</b></p>
-          <p>Checkout the new release notes <a href="/docs/CHANGELOG" >--> here</a> </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption style={{ minHeight: '120px' }}>
-          <h4>🚀 NEW 🚀</h4>
-          <p><b>GitHub Source Control Integration</b></p>
-          <p>Harmonix on AWS is now integrated with both GitHub & Gitlab Source control <br />You can even extend it to more git providers. Read more <a href="/docs/integration/git" >-->here</a></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption style={{ minHeight: '120px' }}>
-          <h4>🚀 NEW 🚀</h4>
-          <p><b>Terraform ECS Provider Template</b></p>
-          <p>A New Terraform template to provision AWS ECS Clusters</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption style={{ minHeight: '120px' }}>
-          <h4>🚀 NEW 🚀</h4>
-          <p><b>GenAI provider and RAG app</b></p>
-          <p>New GenAI Provider for development of GenAI Applications <br /> A new RAG demo app to get you started quickly <a href="/docs/category/generative-ai">Click here for more details</a></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption style={{ minHeight: '120px' }}>
-          <h4>🚀 NEW 🚀</h4>
-          <p><b>AWS SecretsManager shared resource</b></p>
-          <p>You can now create shared secrets on AWS and easily bind them to applications</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption style={{ minHeight: '120px' }}>
-          <h4>🚀 NEW 🚀</h4>
-          <p><b>AWS Control Tower & AFT Integration</b></p>
-          <p>Step by Step <a href="/docs/integration/control-tower-and-aft" >Documentation</a> how to integrate Harmonix on AWS <br /> with AWS Control Tower and AFT (Account Factory for Terraform)</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+  <Carousel data-bs-theme="dark" interval={4200} pause={'hover'} touch={true} title='' >
+  <Carousel.Item>
+    <Carousel.Caption style={{minHeight:'120px'}}>
+      <h4>🚀 NEW RELEASE 0.3.4 🚀</h4>
+      <p><b>V0.3.4</b></p>
+      <p>Checkout the new release notes <a href="/docs/CHANGELOG" > --&gt; here</a> </p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Carousel.Caption style={{minHeight:'120px'}}>
+      <h4>🚀 NEW 🚀</h4>
+      <p><b>GitHub Source Control Integration</b></p>
+      <p>Harmonix on AWS is now integrated with both GitHub & Gitlab Source control <br/>You can even extend it to more git providers. Read more <a href="/docs/integration/git" >--&gt; here</a></p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Carousel.Caption style={{minHeight:'120px'}}>
+      <h4>🚀 NEW 🚀</h4>
+      <p><b>Terraform ECS Provider Template</b></p>
+      <p>A New Terraform template to provision AWS ECS Clusters</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Carousel.Caption style={{minHeight:'120px'}}>
+      <h4>🚀 NEW 🚀</h4>
+      <p><b>GenAI provider and RAG app</b></p>
+      <p>New GenAI Provider for development of GenAI Applications <br/> A new RAG demo app to get you started quickly <a href="/docs/category/generative-ai">Click here for more details</a></p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Carousel.Caption style={{minHeight:'120px'}}>
+      <h4>🚀 NEW 🚀</h4>
+      <p><b>AWS SecretsManager shared resource</b></p>
+      <p>You can now create shared secrets on AWS and easily bind them to applications</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Carousel.Caption style={{minHeight:'120px'}}>
+      <h4>🚀 NEW 🚀</h4>
+      <p><b>AWS Control Tower & AFT Integration</b></p>
+      <p>Step by Step <a href="/docs/integration/control-tower-and-aft" >Documentation</a> how to integrate Harmonix on AWS <br/> with AWS Control Tower and AFT (Account Factory for Terraform)</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  
 
 
 

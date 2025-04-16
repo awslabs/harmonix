a07e477777911c7eb462cabec042d0a79463201c
Anthony Watson <watsoac@amazon.com>
2025-04-16 12:43:05 -0700

    fix: stop_local killing backstage

--- build-script/local-runners.sh
+++ build-script/local-runners.sh
@@ -56,7 +56,7 @@ stop_local () {  ## Stop all running processes for local development
 	docker compose -f $appRootDir/config/docker-compose.yml down
 	echo "PostgreSQL container(s) stopped"
 	echo "Stopping Backstage"
-	kill $(lsof -ti :3000)
-	kill $(lsof -ti :7007)
+	kill $(lsof -ti :3000) >/dev/null 2>&1
+	kill $(lsof -ti :7007) >/dev/null 2>&1
 	echo "Backstage stopped"
 }

139c70cc93133cd1be69caa7d9c6dd117e4361d5
Anthony Watson <watsoac@amazon.com>
2025-04-16 12:22:17 -0700

    fix: backstage docker image build

--- build-script/build-backstage.sh
+++ build-script/build-backstage.sh
@@ -7,9 +7,18 @@ configDir=${scriptDir}/../config
 
 source ${configDir}/.env
 
-yarn --cwd  $backstageDir tsc
-yarn --cwd  $backstageDir build:all
-# yarn build-image
+cd "$backstageDir"
+
+yarn install --immutable
+
+# tsc outputs type definitions to dist-types/ in the repo root, which are then consumed by the build
+yarn tsc
+
+# Build the backend, which bundles it all up into the packages/backend/dist folder.
+yarn build:backend
+
 export DOCKER_BUILDKIT=1
-docker build . -f ${configDir}/aws-production.Dockerfile --tag opa-backstage
+docker build --platform linux/amd64 . -f ${configDir}/aws-production.Dockerfile --tag opa-backstage
 echo "Backstage app build finished"
+
+cd -

b3814183d02b8be3683ba9640147bacd78563278
Zahi Ben Shabat <zshabat@amazon.com>
2025-04-17 15:31:45 -0400

    homepage

---
+++ backstage-plugins/plugins/harmonix-frontend/src/demo/HarmonixLogoIcon.tsx
@@ -0,0 +1,40 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  svg: {
+    width: 'auto',
+    height: 18,
+  },
+  st0path: {
+    fill: '#FFFFFF',
+  },
+  st1path: {
+    fill: '#FFFFFF',
+    fillRule: 'evenodd',
+    clipRule: 'evenodd',
+  },
+});
+export const HarmonixLogoIcon = () => {
+  const classes = useStyles();
+
+  return (
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xmlSpace="preserve" className={classes.svg}>  <image id="image0" width="16" height="16" x="0" y="0"
+    xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAIGNIUk0AAHomAACAhAAA+gAAAIDo
+AAB1MAAA6mAAADqYAAAXcJy6UTwAAADAUExURQAAAAAxgAAzegAxggAwgQAxgQAxgwAyggAygQAy
+ggAxgQAxgQAwgAAygQAxggAwgwAwgQAxgQAwggAxgAAxgQAygwAxggAwgQAygQAxgQAwgQAxgQAy
+gQAygQAxgQAxgQAwggAxgQAxgQAxggAygQAxgAAxgQAwgAAxgQAxgAAwgQAyggA1gAAwgQAwgAAx
+gQAxgQBAgAAwggAygwAxggA3kQA3kgA0igAzhgAyhAA7mwAzhQA2jwA4lAA0if////ZXM5wAAAA1
+dFJOUwCHGWhFc21cgDPckkBNOSXDiGA09md4npr4efdxkIzzeqbqcnsa55nakVU9Iq4ghlkEaky3
+Ws+rqwAAAAFiS0dEPz5jMHUAAAAHdElNRQfpBBEPCyAzfMjuAAAAfElEQVQY062PVxKCUBAEG3NC
+RVExgYg554R6/2M578cTsB+9U101W7WQwFgpIZ2BbE4hX+Ba1C6VoXKzqd5tHjWJuiM0nk23BW67
+43ndnmk7L0vsD4a+H4yMCMeRGP8rk3g6m8PbHF0sYfVZs/lGbHcS+wPH8KRwviTxyg8i6QjDfn+r
+VwAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNS0wNC0xN1QxNToxMTozMiswMDowMGNeX7cAAAAldEVY
+dGRhdGU6bW9kaWZ5ADIwMjUtMDQtMTdUMTU6MTE6MzIrMDA6MDASA+cLAAAAKHRFWHRkYXRlOnRp
+bWVzdGFtcAAyMDI1LTA0LTE3VDE1OjExOjMyKzAwOjAwRRbG1AAAAABJRU5ErkJggg==" />
+</svg>
+  );
+};
